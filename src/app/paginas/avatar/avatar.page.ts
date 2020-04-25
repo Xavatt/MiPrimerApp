fb92246421b352f79ea0692370d0e1d22c291776
Xavier Guinto Rios <xavierguinto3@outlook.es>
2020-04-24 20:16:32 -0500

    Gato ion-chip
    Se hizo el mismo codigo del commit anterior pero para que ahora funcione con Gatos

--- src/app/paginas/avatar/avatar.page.ts
+++ src/app/paginas/avatar/avatar.page.ts
@@ -1,13 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { AlertController } from '@ionic/angular';
 
-interface ListaComponente{
+interface ListaComponenteP{
+  name: string;
+  icon: string;  
+}
+
+interface ListaComponenteG{
   name: string;
   icon: string;  
 }
 
 interface Perro{
-  nombre: string;
+  nombreP: string;
+}
+
+interface Gato{
+  nombreG: string;
 }
 
 @Component({
@@ -18,17 +27,29 @@ interface Perro{
 export class AvatarPage implements OnInit {
 
   
-nombre: string;
+nombreP: string;
 conteoPerros = 0;
+
+nombreG: string;
+conteoGatos = 0;
 
 Perro: Perro[] = []
 listadoPerro: Array<Perro> = this.Perro;
 
+Gato: Gato[] = []
+listadoGato: Array<Gato> = this.Gato;
 
-  listaPerro: ListaComponente[] = [
+  listaPerro: ListaComponenteP[] = [
     {
       name: 'Avatar Perrito',
       icon: '/assets/perrito.png'
+    }
+  ]
+
+  listaGato: ListaComponenteG[] = [
+    {
+      name: 'Avatar Gato',
+      icon: '/assets/gatito.png'
     }
   ]
 
@@ -37,12 +58,21 @@ listadoPerro: Array<Perro> = this.Perro;
     console.log('Confirm Ok');          
     this.conteoPerros = this.conteoPerros + 1;
     console.log(this.conteoPerros);
-    this.listadoPerro.push({nombre: this.nombre})
+    this.listadoPerro.push({nombreP: this.nombreP})
+  }
+
+
+  incrementoGato()
+  {    
+    console.log('Confirm Ok');          
+    this.conteoGatos = this.conteoGatos + 1;
+    console.log(this.conteoGatos);
+    this.listadoGato.push({nombreG: this.nombreG})
   }
 
   constructor(public alertController: AlertController) { }
 
-  async presentAlertPrompt() {
+  async PromptPerro() {
     
     const alert = await this.alertController.create({
       header: 'Perros!',
@@ -66,8 +96,42 @@ listadoPerro: Array<Perro> = this.Perro;
           text: 'Ok',
           handler: (datos) => {
             console.log('Confirm Ok',datos);
-            this.nombre = datos.txtNombre;           
+            this.nombreP = datos.txtNombre;           
             this.incrementoPerro();
+          }
+        }
+      ]
+    });
+   
+    await alert.present();
+  }
+
+  async PromptGato() {
+    
+    const alert = await this.alertController.create({
+      header: 'Gatos!',
+      backdropDismiss: false,
+      inputs: [
+        {
+          name: 'txtNombre',
+          type: 'text',
+          placeholder: 'Dame un nombre'                       
+        },        
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: () => {
+            console.log('Confirm Cancel');
+          }
+        }, {
+          text: 'Ok',
+          handler: (datos) => {
+            console.log('Confirm Ok',datos);
+            this.nombreG = datos.txtNombre;           
+            this.incrementoGato();
           }
         }
       ]

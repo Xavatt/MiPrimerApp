cc49ee69fed7da776d40836262a8a333920bac6a
Xavier Guinto Rios <xavierguinto3@outlook.es>
2020-04-24 19:58:21 -0500

    Agregar ion-chips por boton
    Se modificaron los archivos para cada que el usuario de click agregue el nombre de un perro y se muestre en pantalla

--- src/app/paginas/avatar/avatar.page.ts
+++ src/app/paginas/avatar/avatar.page.ts
@@ -1,4 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+
+interface ListaComponente{
+  name: string;
+  icon: string;  
+}
+
+interface Perro{
+  nombre: string;
+}
 
 @Component({
   selector: 'app-avatar',
@@ -7,9 +17,68 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AvatarPage implements OnInit {
 
-  constructor() { }
+  
+nombre: string;
+conteoPerros = 0;
+
+Perro: Perro[] = []
+listadoPerro: Array<Perro> = this.Perro;
+
+
+  listaPerro: ListaComponente[] = [
+    {
+      name: 'Avatar Perrito',
+      icon: '/assets/perrito.png'
+    }
+  ]
+
+  incrementoPerro()
+  {    
+    console.log('Confirm Ok');          
+    this.conteoPerros = this.conteoPerros + 1;
+    console.log(this.conteoPerros);
+    this.listadoPerro.push({nombre: this.nombre})
+  }
+
+  constructor(public alertController: AlertController) { }
+
+  async presentAlertPrompt() {
+    
+    const alert = await this.alertController.create({
+      header: 'Perros!',
+      backdropDismiss: false,
+      inputs: [
+        {
+          name: 'txtNombre',
+          type: 'text',
+          placeholder: 'Dame un nombre'                       
+        },        
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: () => {
+            console.log('Confirm Cancel');
+          }
+        }, {
+          text: 'Ok',
+          handler: (datos) => {
+            console.log('Confirm Ok',datos);
+            this.nombre = datos.txtNombre;           
+            this.incrementoPerro();
+          }
+        }
+      ]
+    });
+   
+    await alert.present();
+  }
 
   ngOnInit() {
   }
+
+
 
 }
